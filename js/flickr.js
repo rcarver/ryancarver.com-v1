6feeb3daaffce7af3de8765edc2b705229aedb3f
Ryan Carver <ryan@typekit.com>
2011-01-02 17:29:47 -0800

    complete rewrite

--- js/flickr.js
+++ js/flickr.js
@@ -8,44 +8,61 @@ $(function() {
   var loading = new LoadingView();
   $('#main').append(loading.render().el);
 
-  var photoSet = new PhotoSet();
-  photoSet.setFlickrOptions({
+  var photoList = new PhotoList({
     flickrId: flickrId,
     flickrTags: tags
   });
 
-  var gallery = new Gallery({
-    photoSet: photoSet
-  });
 
-  var grid = new GalleryGridView({ model: gallery, el: '#main #grid' });
-  var single = new GallerySingleView({ model: gallery, el: '#main #single' });
-  var controller = new GalleryController(gallery);
-
-  new GalleryScrollView({ model: gallery, el: '#main #grid' });
-
-  photoSet.fetch({
+  photoList.fetch({
     success: function() {
-      controller.start();
-      grid.render();
-      loading.remove();
+      var photoSet = photoList.getPhotoSet();
+      var loaded = photoSet.length;
+
+      var gallery = new Gallery({
+        photoSet: photoSet
+      });
+
+
+      photoSet.each(function(photo) {
+        photo.fetch({
+          success: function() {
+            if (--loaded == 0) {
+
+              new GalleryLayout({ model: gallery, el: $('#image') });
+              new GalleryInteration({ model: gallery, el: $('#image') });
+
+               new GalleryController(gallery);
+              gallery.set({ loaded: true });
+
+              loading.remove();
+            }
+          }
+        });
+      });
     }
   });
 
 });
 
 
-/*
-* Represents a single photo.
-*/
-var Photo = Backbone.Model.extend({
-  getLargeUrl: function() {
-    var item = this.get('item');
-    return item.media.m.replace(/m\.jpg$/, 'b.jpg');
+function paddingForCentering(item, space) {
+  var width = item.outerWidth();
+  var windowWidth = space.width();
+  return (windowWidth - width) / 2;
+}
+
+var PhotoList = Backbone.Model.extend({
+  url: function() {
+    return 'http://api.flickr.com/services/feeds/photos_public.gne?id=' + this.get('flickrId')+ '&tags=' + this.get('flickrTags').join(',') + '&format=json&jsoncallback=?';
   },
-  getSmallUrl: function() {
-    var item = this.get('item');
-    return item.media.m.replace(/m\.jpg$/, 'z.jpg');
+
+  getPhotoSet: function() {
+    var photoSet = new PhotoSet;
+    _.each(this.get('items'), function(item) {
+      photoSet.add(new Photo(item));
+    });
+    return photoSet;
   }
 });
 
@@ -54,26 +71,39 @@ var Photo = Backbone.Model.extend({
 */
 var PhotoSet = Backbone.Collection.extend({
   model: Photo,
-  setFlickrOptions: function(options) {
-    this.flickrId = options.flickrId;
-    this.flickrTags = options.flickrTags || [];
-  },
-  url: function() {
-    return 'http://api.flickr.com/services/feeds/photos_public.gne?id=' + this.flickrId + '&tags=' + this.flickrTags.join(',') + '&format=json&jsoncallback=?';
-  },
-  parse: function(response) {
-    return _.map(response.items, function(item) {
-      return { item: item };
-    });
-  }
 });
+
+/*
+* Represents a single photo.
+*/
+var Photo = Backbone.Model.extend({
+  initialize: function(attributes) {
+    var extractedId = this.get('link').match(/\/([^\/]+)\/$/)[1];
+    this.set({ id: extractedId });
+  },
+
+  url: function() {
+    var template = "http://api.flickr.com/services/rest/?method=flickr.photos.getSizes&api_key=3eff5a940d905ad02b98412d701909f3&photo_id=<%= photo_id %>&format=json&jsoncallback=?";
+    return _.template(template, { photo_id: this.id });
+  },
+
+  parse: function(response) {
+    return response.sizes;
+  },
+
+  getLarge: function() {
+    var data = this.get('size');
+    return _.detect(data, function(size) { return size.label == "Medium 640" });
+  },
+});
+
 
 /*
 * Models a simple gallery where only one photo is visible at a time.
 */
 var Gallery = Backbone.Model.extend({
   initialize: function() {
-    this.set({ index: 0, view: false });
+    this.set({ index: null, loaded: false });
   },
   getPhoto: function() {
     return this.get('photoSet').at(this.get('index'));
@@ -96,35 +126,31 @@ var Gallery = Backbone.Model.extend({
 */
 var GalleryController = Backbone.Controller.extend({
   initialize: function(gallery) {
-    _.bindAll(this, "updateGallery");
+    _.bindAll(this, "saveState", "start");
     this.gallery = gallery;
-    this.gallery.bind('change', this.updateGallery);
+    this.gallery.bind('change:index', this.saveState);
+    this.gallery.bind('change:loaded', this.start);
   },
   routes: {
-    '': "showGrid",
-    'photo/:index': "showView"
+    'photo/:index': "setPhotoIndex"
   },
   start: function() {
-    Backbone.history.start();
-  },
-  showGrid: function() {
-    this.gallery.set({ view: false });
-  },
-  showView: function(index) {
-    this.gallery.set({ index: parseInt(index), view: true });
-  },
-  updateGallery: function() {
-    if (this.gallery.get('view')) {
-      this.saveLocation("photo/" + this.gallery.get('index'));
+    this.gallery.set({ scroll: false });
+    if (document.location.href.indexOf('#') >= 0) {
+      Backbone.history.start();
     } else {
-      this.saveLocation();
+      this.setPhotoIndex(0);
     }
+    this.gallery.set({ scroll: true });
+  },
+  setPhotoIndex: function(index) {
+    this.gallery.set({ index: parseInt(index) });
+  },
+  saveState: function() {
+    this.saveLocation("photo/" + this.gallery.get('index'));
   }
 });
 
-/*
-* A view for the initial loading message.
-*/
 var LoadingView = Backbone.View.extend({
 
   tagName: "div",
@@ -136,165 +162,125 @@ var LoadingView = Backbone.View.extend({
   }
 });
 
-/*
-* A view representing the gallery as a whole.
-*/
-var GallerySingleView = Backbone.View.extend({
-
-  events: {
-    'click .photo': "next",
-    'click .close': "close",
-  },
+var GalleryLayout = Backbone.View.extend({
 
   initialize: function() {
-    _.bindAll(this, "render", "next", "close");
-    this.model.bind('change', this.render);
-    this.currentView = null;
+    _.bindAll(this, 'render');
+    this.model.bind('change:loaded', this.render);
   },
 
   render: function() {
-    $(this.el).html('');
-    if (this.currentView) {
-      this.currentView.remove();
-    }
-    var photo = this.model.getPhoto();
-    var view = new PhotoView({ model: photo, size: 'large' });
-    $(view.render().el).appendTo(this.el);
-    $('<div class="close">Close</div>').appendTo(this.el);
-    this.currentView = view;
+    var wrapper = this.$('.wrapper');
 
-    if (this.model.get('view')) {
-      $(this.el).show();
-    } else {
-      $(this.el).hide();
+    var photos = this.model.getPhotos();
+
+    var table = $('<table/>').appendTo(wrapper);
+    var row = $('<tr/>').appendTo(table);
+    var template = this.$('.template').text();
+
+    _.each(photos, function(photo, index) {
+      var data = photo.getLarge();
+      var src = _.template(template, { src: data.source, width: data.width, height: data.height });
+      var cell = $('<td/>').append(src);
+      row.append(cell);
+    });
+
+    // add space around the images based on the window size.
+    var images = this.$('img');
+    var avgWidth = _.inject(images, function(memo, img) { return memo + $(img).width(); }, 0) / images.length;
+    var space = (this.el.width() - avgWidth) / 2;
+    var padding = Math.max(space / 3, 30);
+    this.$('.frame').css('padding-left', padding).css('padding-right', padding);
+    this.$('.frame:first').css('padding-left', 0)
+    this.$('.frame:last').css('padding-right', 0)
+
+    // move the first image right so that it starts out centered.
+    var img = this.$('img').eq(0);
+    var position = paddingForCentering(img, this.el);
+    this.$('td').eq(0).css('padding-left', position);
+
+    // move the last image left so that ends centered.
+    var img = this.$('img').eq(photos.length - 1);
+    var position = paddingForCentering(img, this.el);
+    this.$('td').eq(photos.length - 1).css('padding-right', position);
+  }
+
+});
+
+var GalleryInteration = Backbone.View.extend({
+
+  events: {
+    'click': "nextPhoto"
+  },
+
+  initialize: function() {
+    _.bindAll(this, "render", "nextPhoto", "setIndexViaScroll", "finishAnimating");
+    this.model.bind('change:index', this.render);
+    this.el.scroll(this.setIndexViaScroll);
+  },
+
+  render: function() {
+    var index = this.model.get('index');
+    var img = this.$('img').eq(index);
+
+    this.$('td').removeClass('current');
+    this.$('td').eq(index).addClass('current');
+
+    if (!this.isScrolling()) {
+      var position = this.leftForCentering(img);
+      if (this.model.get('scroll')) {
+        if (this.animating) {
+          this.el.stop(true);
+        }
+        this.animating = true;
+        this.el.animate({ scrollLeft: position }, 500, this.finishAnimating);
+      } else {
+        this.el.scrollLeft(position);
+      }
     }
   },
 
-  next: function() {
+  isScrolling: function() {
+    return this.scrollingAt && (+new Date) - this.scrollingAt < 100;
+  },
+
+  finishAnimating: function() {
+    this.animating = false;
+  },
+
+  nextPhoto: function(e) {
     this.model.nextIndex();
   },
 
-  close: function() {
-    this.model.set({ view: false });
-  }
-});
+  setIndexViaScroll: function() {
+    if (this.animating) {
+      return;
+    }
+    this.scrollingAt = +new Date;
 
-/*
-* A view representing the gallery as a whole.
-*/
-var GalleryGridView = Backbone.View.extend({
-
-  events: {
-    'click': "showPhoto"
-  },
-
-  initialize: function() {
-    _.bindAll(this, "render", "showPhoto");
-    this.model.bind('change', this.render);
-  },
-
-  render: function() {
-    var el = this.el;
-    $(el).html('');
-    var photos = this.model.getPhotos();
+    var model = this.model;
+    var windowWidth = this.el.width();
+    var center = windowWidth / 2;
     var currentIndex = this.model.get('index');
-    var table = $('<table><tr/></table>').appendTo(el);
-    _.each(photos, function(photo, index) {
-      var current = index == currentIndex;
-      var url = photo.getSmallUrl();
-      var img = $('<img/>').attr('src', url);
-      var cell = $('<td/>').append(img);
-      table.append(cell);
-      if (current) {
-        cell.addClass('current');
+
+    _.each(this.$('img'), function(e, index) {
+      if (index != currentIndex) {
+        var img = $(e);
+        var left = img.offset().left;
+        var width = img.outerWidth();
+        if (center > left && center < left + width) {
+          model.set({ index: index });
+        }
       }
     });
   },
 
-  showPhoto: function(e) {
-    // TODO: set the real index based on the clicked element.
-    this.model.set({ index: 2, view: true });
+ leftForCentering: function(item) {
+    var left = item.position().left;
+    var width = item.outerWidth();
+    var windowWidth = this.el.width();
+    return left - ((windowWidth - width) / 2);
   }
+
 });
-
-/*
-* A view representing the gallery as a whole.
-*/
-var GalleryScrollView = Backbone.View.extend({
-
-  initialize: function() {
-    _.bindAll(this, "render", "setImageViaScroll");
-    this.model.bind('change', this.render);
-    this.animating = false;
-    $(window).scroll(this.setImageViaScroll);
-  },
-
-  render: function() {
-    if (this.animating) {
-      $('body').stop(true);
-    }
-    this.animating = true;
-    var self = this;
-    var el = $(this.el);
-    var currentIndex = this.model.get('index');
-    var img = el.find('img').eq(currentIndex);
-    var left = img.offset().left;
-    var width = img.outerWidth();
-    var windowWidth = $('body').width();
-    $('body').animate({ scrollLeft: left + (width / 2) - (windowWidth / 2) }, 500, function() {
-      self.animating = false;
-    });
-  },
-
-  setImageViaScroll: function() {
-    if (!this.animating) {
-      var scrollPosition = $('body').scrollLeft();
-      var windowWidth = $('body').width();
-      var center = scrollPosition + (windowWidth / 2);
-      var el = $(this.el);
-      var model = this.model;
-      var currentIndex = this.model.get('index');
-      _.each(el.find('img'), function(e, index) {
-        if (index != currentIndex) {
-          var img = $(e);
-          var left = img.offset().left;
-          var width = img.outerWidth();
-          if (center > left && center < left + width) {
-            model.set({ index: index });
-          }
-        }
-      });
-    }
-  }
-});
-
-/*
-* A view representing a single photo in a gallery.
-*/
-var PhotoView = Backbone.View.extend({
-
-  tagName: 'div',
-  className: 'photo',
-
-  initialize: function() {
-    this.size = this.options.size || 'small';
-    this.current = this.options.current;
-    _.bindAll(this, "render");
-    this.model.bind('change', this.render);
-  },
-
-  render: function() {
-    if (this.size == 'large') {
-      var url = this.model.getLargeUrl();
-    } else {
-      var url = this.model.getSmallUrl();
-    }
-    var img = $('<img/>').attr('src', url).appendTo(this.el);
-    if (this.current) {
-      img.addClass('current');
-    }
-    return this;
-  }
-});
-
 
